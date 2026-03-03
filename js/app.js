646919e598a54f18ddc74863a98fa3cf123819c6
phyowaitunn <pj7srsjb22@privaterelay.appleid.com>
2026-03-04 01:00:56 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-let slides=document.querySelectorAll(".slide");
-let dots=document.querySelectorAll(".dot");
+let slides = document.querySelectorAll(".slide");
+let dots = document.querySelectorAll(".dot");
 
-let index=0;
+let index = 0;
 
+/* show slide */
 function showSlide(i){
-
 slides.forEach(s=>s.classList.remove("active"));
 dots.forEach(d=>d.classList.remove("active"));
 
@@ -12,14 +12,48 @@ slides[i].classList.add("active");
 dots[i].classList.add("active");
 }
 
+/* buttons */
 function next(){
+if(index < slides.length-1){
 index++;
-if(index>=slides.length) index=slides.length-1;
 showSlide(index);
+}
 }
 
 function prev(){
+if(index > 0){
 index--;
-if(index<0) index=0;
 showSlide(index);
+}
+}
+
+/* =====================
+   SWIPE SYSTEM
+===================== */
+
+let startX = 0;
+let endX = 0;
+
+document.addEventListener("touchstart",(e)=>{
+startX = e.touches[0].clientX;
+});
+
+document.addEventListener("touchend",(e)=>{
+endX = e.changedTouches[0].clientX;
+handleSwipe();
+});
+
+function handleSwipe(){
+
+let diff = startX - endX;
+
+if(Math.abs(diff) > 50){
+
+if(diff > 0){
+next();     // swipe left
+}else{
+prev();     // swipe right
+}
+
+}
 }
