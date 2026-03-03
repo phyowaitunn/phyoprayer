7124159df9fe479c390b78d65650c5e0cd2747e9
phyowaitunn <pj7srsjb22@privaterelay.appleid.com>
2026-03-04 01:53:37 +0800

    Update prayer.js

--- js/prayer.js
+++ js/prayer.js
@@ -1,74 +1,57 @@
-// Malaysia Penang Coordinates
-const lat = 5.4141;
-const lon = 100.3288;
+const lat=5.4141;
+const lon=100.3288;
 
-// GET PRAYER TIME
 fetch(`https://api.aladhan.com/v1/timings?latitude=${lat}&longitude=${lon}&method=3`)
-.then(res => res.json())
-.then(data => {
+.then(res=>res.json())
+.then(data=>{
 
-let t = data.data.timings;
-let date = data.data.date;
+let t=data.data.timings;
+let d=data.data.date;
 
-// Gregorian Date
-document.getElementById("date").innerText =
-date.readable;
+document.getElementById("todayDate")
+.innerText=d.readable;
 
-// Hijri Date
-document.getElementById("hijri").innerText =
-date.hijri.day + " " +
-date.hijri.month.en +
-" " + date.hijri.year + " H";
+document.getElementById("fajr").innerText=t.Fajr;
+document.getElementById("dhuhr").innerText=t.Dhuhr;
+document.getElementById("asr").innerText=t.Asr;
+document.getElementById("maghrib").innerText=t.Maghrib;
+document.getElementById("isha").innerText=t.Isha;
 
-// Location
-document.getElementById("location").innerText =
-"Pulau Pinang - Perai";
-
-// Prayer list
-let prayers = [
-["Imsak", t.Imsak],
-["Fajr", t.Fajr],
-["Dhuhr", t.Dhuhr],
-["Asr", t.Asr],
-["Maghrib", t.Maghrib],
-["Isha", t.Isha]
+let prayers=[
+["Fajr",t.Fajr],
+["Dhuhr",t.Dhuhr],
+["Asr",t.Asr],
+["Maghrib",t.Maghrib],
+["Isha",t.Isha]
 ];
 
-let now = new Date();
-
-let nextName="";
-let nextTime="";
+let now=new Date();
+let nextName="",nextTime="";
 
 for(let p of prayers){
 
-let timeParts=p[1].split(":");
+let time=p[1].split(":");
 
-let prayerTime=new Date();
-prayerTime.setHours(timeParts[0]);
-prayerTime.setMinutes(timeParts[1]);
-prayerTime.setSeconds(0);
+let pt=new Date();
+pt.setHours(time[0]);
+pt.setMinutes(time[1]);
 
-if(prayerTime>now){
+if(pt>now){
 nextName=p[0];
 nextTime=p[1];
 break;
 }
 }
 
-// Show next prayer
 document.getElementById("nextPrayer")
-.innerText = nextName + ", " + nextTime;
+.innerText=`${nextName} - ${nextTime}`;
 
-
-// COUNTDOWN
 setInterval(()=>{
-
-let timeParts=nextTime.split(":");
+let time=nextTime.split(":");
 
 let target=new Date();
-target.setHours(timeParts[0]);
-target.setMinutes(timeParts[1]);
-target.setSeconds(0);
+target.setHours(time[0]);
+target.setMinutes(time[1]);
 
 let diff=target-new Date();
 
@@ -76,13 +59,8 @@ let h=Math.floor(diff/3600000);
 let m=Math.floor(diff/60000)%60;
 
 document.getElementById("countdown")
-.innerText=
-nextName+" in "+h+" hour "+m+" minute";
+.innerText=`Starts in ${h}h ${m}m`;
 
 },1000);
 
-})
-.catch(err=>{
-console.log(err);
-alert("Prayer API failed");
 });
