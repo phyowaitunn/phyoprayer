a8a4815e640404c609f5f89a23f1fe83e56d3f2d
phyowaitunn <pj7srsjb22@privaterelay.appleid.com>
2026-03-04 01:28:45 +0800

    Update prayer.js

--- js/prayer.js
+++ js/prayer.js
@@ -62,3 +62,69 @@ document.getElementById("countdown")
 },1000);
 
 });
+
+
+/*prayer real data*/
+
+const zone="PNG01"; // Penang
+
+fetch(`https://api.waktusolat.app/v2/solat/${zone}`)
+.then(r=>r.json())
+.then(data=>{
+
+let today=data.prayers[0];
+
+// DATE
+let d=new Date(today.date);
+document.getElementById("date").innerText=
+d.toLocaleDateString("en-GB",
+{day:"2-digit",month:"long",year:"numeric"});
+
+// HIJRI
+document.getElementById("hijri").innerText=
+today.hijri+" H";
+
+// LOCATION
+document.getElementById("location").innerText=
+data.zone;
+
+// PRAYER TIMES
+let prayers=[
+["Imsak",today.imsak],
+["Fajr",today.fajr],
+["Dhuhr",today.dhuhr],
+["Asr",today.asr],
+["Maghrib",today.maghrib],
+["Isha",today.isha]
+];
+
+let now=new Date();
+let nextName="";
+let nextTime="";
+
+for(let p of prayers){
+let t=new Date(today.date+" "+p[1]);
+if(t>now){
+nextName=p[0];
+nextTime=p[1];
+break;
+}
+}
+
+document.getElementById("nextPrayer")
+.innerText=`${nextName}, ${nextTime}`;
+
+// COUNTDOWN
+setInterval(()=>{
+let target=new Date(today.date+" "+nextTime);
+let diff=target-new Date();
+
+let h=Math.floor(diff/3600000);
+let m=Math.floor(diff/60000)%60;
+
+document.getElementById("countdown")
+.innerText=`${nextName} in ${h} hour ${m} minute`;
+
+},1000);
+
+});
