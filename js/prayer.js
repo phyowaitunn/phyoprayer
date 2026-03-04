e630c5ddba4b57adbef564147f0d9f485c59da50
phyowaitunn <pj7srsjb22@privaterelay.appleid.com>
2026-03-04 23:44:29 +0800

    Update prayer.js

--- js/prayer.js
+++ js/prayer.js
@@ -1,82 +1,69 @@
-const lat=5.4141;
-const lon=100.3288;
-
-function greeting(){
-let h=new Date().getHours();
-
-let text="Assalamu Alaikum";
-
-if(h<12) text="Good Morning ☀️";
-else if(h<18) text="Good Afternoon 🌤";
-else text="Good Evening 🌙";
-
-document.getElementById("greeting").innerText=text;
-}
-
-greeting();
+const lat = 5.4141;
+const lon = 100.3288;
 
 fetch(`https://api.aladhan.com/v1/timings?latitude=${lat}&longitude=${lon}&method=3`)
-.then(res=>res.json())
-.then(data=>{
+.then(res => res.json())
+.then(data => {
 
-let t=data.data.timings;
-let d=data.data.date;
+let t = data.data.timings;
 
-document.getElementById("todayDate").innerText=d.readable;
-
-fajr.innerText=t.Fajr;
-dhuhr.innerText=t.Dhuhr;
-asr.innerText=t.Asr;
-maghrib.innerText=t.Maghrib;
-isha.innerText=t.Isha;
-
-let prayers=[
-["Fajr",t.Fajr],
-["Dhuhr",t.Dhuhr],
-["Asr",t.Asr],
-["Maghrib",t.Maghrib],
-["Isha",t.Isha]
+let prayers = [
+["Fajr", t.Fajr],
+["Dhuhr", t.Dhuhr],
+["Asr", t.Asr],
+["Maghrib", t.Maghrib],
+["Isha", t.Isha]
 ];
 
-let now=new Date();
-let nextName,nextTime,target;
+let now = new Date();
 
-for(let p of prayers){
+let nextName = "";
+let nextTime = "";
+let target;
 
-let time=p[1].split(":");
+for (let p of prayers) {
 
-let pt=new Date();
+let time = p[1].split(":");
+
+let pt = new Date();
 pt.setHours(time[0]);
 pt.setMinutes(time[1]);
+pt.setSeconds(0);
 
-if(pt>now){
-nextName=p[0];
-nextTime=p[1];
-target=pt;
+if (pt > now) {
+nextName = p[0];
+nextTime = p[1];
+target = pt;
 break;
 }
+
 }
 
-nextPrayer.innerText=
-`${nextName} • ${nextTime}`;
+document.getElementById("nextPrayer").innerText =
+nextName + " • " + nextTime;
 
-setInterval(()=>{
 
-let diff=target-new Date();
+/* COUNTDOWN + PROGRESS */
 
-let h=Math.floor(diff/3600000);
-let m=Math.floor(diff/60000)%60;
-let s=Math.floor(diff/1000)%60;
+setInterval(() => {
 
-countdown.innerText=
-`${h}h ${m}m ${s}s remaining`;
+let diff = target - new Date();
 
-let total=6*60*60*1000;
-let progress=100-(diff/total*100);
+let h = Math.floor(diff / 3600000);
+let m = Math.floor(diff / 60000) % 60;
+let s = Math.floor(diff / 1000) % 60;
 
-document.getElementById("progressBar")
-.style.width=progress+"%";
+document.getElementById("countdown").innerText =
+h + "h " + m + "m " + s + "s remaining";
 
-},1000);
+/* progress bar */
+
+let total = 6 * 60 * 60 * 1000;
+let progress = 100 - (diff / total * 100);
+
+document.getElementById("progressBar").style.width =
+progress + "%";
+
+}, 1000);
 
 });
