34f81a52451cfe13536248b36fa5ebd3ee30b00b
phyowaitunn <pj7srsjb22@privaterelay.appleid.com>
2026-03-05 21:32:12 +0800

    Update quran.js

--- js/quran.js
+++ js/quran.js
@@ -37,3 +37,71 @@ ${surah.englishNameTranslation}
 });
 
 });
+
+
+
+
+
+let surahData = [];
+
+fetch("https://api.alquran.cloud/v1/surah")
+.then(res=>res.json())
+.then(data=>{
+
+surahData = data.data;
+showSurah(surahData);
+
+});
+
+function showSurah(list){
+
+const container = document.getElementById("surahList");
+
+container.innerHTML="";
+
+list.forEach(surah=>{
+
+container.innerHTML+=`
+
+<a href="surah.html?number=${surah.number}" class="surah-card">
+
+<div class="surah-number">
+${surah.number}
+</div>
+
+<div class="surah-text">
+
+<div class="surah-name">
+${surah.englishName}
+</div>
+
+<div class="surah-arabic">
+${surah.name}
+</div>
+
+<div class="surah-translation">
+${surah.englishNameTranslation}
+</div>
+
+</div>
+
+</a>
+
+`;
+
+});
+
+}
+
+document.getElementById("searchSurah")
+.addEventListener("input",e=>{
+
+const text = e.target.value.toLowerCase();
+
+const filtered = surahData.filter(s=> 
+s.englishName.toLowerCase().includes(text)
+);
+
+showSurah(filtered);
+
+});
